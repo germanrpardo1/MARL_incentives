41a572e1570e8940cbf0f3016eacd5479eab3174
Germán Pardo González <g.r.pardo-gonzalez@lse.ac.uk>
2025-11-06 22:30:31 +0000

    update scripts names

--- scripts/dqn_continuous_state_exp_replay.py
+++ scripts/dqn_continuous_state_exp_replay.py
@@ -156,7 +156,7 @@ def main(config, total_budget: int) -> None:
 
 if __name__ == "__main__":
     # Load config
-    config_file = ut.load_config(path="scripts/run_incentives_budget_state.yaml")
+    config_file = ut.load_config(path="scripts/dqn_continuous_state_exp_replay.yaml")
 
     # Loop for different budgets
     for tot_budget in config_file["total_budget"]:
--- scripts/qlearning_no_state.py
+++ scripts/qlearning_no_state.py
@@ -123,7 +123,7 @@ def main(config, total_budget: int) -> None:
 
 if __name__ == "__main__":
     # Load config
-    config_file = ut.load_config(path="scripts/run_incentives_qlearning.yaml")
+    config_file = ut.load_config(path="scripts/qlearning_no_state.yaml")
 
     # Loop for different budgets
     for tot_budget in config_file["total_budget"]:
