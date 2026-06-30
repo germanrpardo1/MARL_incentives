2415ea421b9e98e3bbcd204ff2f4cbbfd2bd5d1d
Germán Pardo González <g.r.pardo-gonzalez@lse.ac.uk>
2026-06-30 21:09:39 +0100

    packages updates

--- scripts/run_schduler.py
+++
@@ -1,8 +0,0 @@
-"""Schedule runs."""
-
-import subprocess
-
-if __name__ == "__main__":
-    subprocess.run(["python", "scripts/qlearning_binary_state_exp_replay.py"])
-
-    subprocess.run(["python", "scripts/qlearning_no_state_exp_replay.py"])
